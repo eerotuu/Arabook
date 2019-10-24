d190efdacb1ca6933927c0842a236c2341585cef
eerotuu <eerotuu@metropolia.fi>
2019-10-24 15:39:33 +0300

    changed posts render, was throwing error when no posts found from db

--- client/src/components/posts.js
+++ client/src/components/posts.js
@@ -19,7 +19,7 @@ class Posts extends Component {
 
     // fetch posts from api and set state
     getList = (url) => {
-        if(typeof url == 'undefined') url = '/api/posts';
+        if(typeof url === 'undefined') url = '/api/posts';
         fetch(url)
             .then(res => res.json())
             .then(postList => this.setState({ postList }))
@@ -28,31 +28,30 @@ class Posts extends Component {
     render() {
         const { postList } = this.state;
 
-        // check if there are any posts to render
-        console.log('postSize ', postList.length);
-        if(typeof postList == 'undefined' || postList.length === 0) {
-            return (
-                <Card style={{marginBottom: "0.5em"}} className="text-center" >
-                    <Card.Body>
-                        <Card.Title>No posts found</Card.Title>
-                        <Card.Text>Try with other search</Card.Text>
-                    </Card.Body>
-                </Card>
-            )
-        }
-
         // map postList into post components
         console.log(postList)
         const posts = () => postList.map(post =>
             <Post key={post._id} post={post} comments={post.comments} getList={this.getList}/>
-        )
+        );
+
+        if(postList.length > 0) {
+            return (
+                <div>
+                    {posts()}
+                </div>
+
+            );
+        }
 
         return (
-            <div>
-                {posts()}
-            </div>
+            <Card style={{marginBottom: "0.5em"}} className="text-center" >
+                <Card.Body>
+                    <Card.Title>No posts found</Card.Title>
+                    <Card.Text>Try with other search</Card.Text>
+                </Card.Body>
+            </Card>
+        )
 
-        );
     }
 }
 
